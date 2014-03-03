8ac49c0bf6f0fd377aa443c3cb8d6fffaa257f8e
root <root@piyush-inspiron-620s.kipl.com>
2014-03-03 10:43:53 +0530

    added comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,18 @@
 class CommentsController < ApplicationController
   
-  def create
-    @topic = Topic.find(params[:topic_id])
-    @comment = @topic.comments.create!(params[:comment])
-    redirect_to :back, :notice => "Comment created!"  
-  end  
+	def create
+    @forum = Forum.find(params[:forum_id])
+    @comment = @forum.comments.create!(params[:comment])
+    redirect_to @forum, :notice => "Comment created!"  
+  end
+
+
 
    
 
   def count 
-  	@topic = Topic.find(params[:topic_id])
-  	@comment = @topic.comments.count(params[:comment]) 
+  	@forum = forum.find(params[:forum_id])
+  	@comment = @forum.comments.count(params[:comment]) 
   end  
 end 
 
