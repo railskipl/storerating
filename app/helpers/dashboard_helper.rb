dcb6394558da951fa8e4e7a75c91a4af758c1390
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-03-03 14:31:08 +0530

    design changes

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,7 +2,10 @@ module DashboardHelper
 	def flag(b)
 	   rating_flag =  RatingFlag.find_by(business_user_id: b.id,user_id: current_user.id).inspect rescue nil 
 	  if rating_flag.nil?
-		link_to "Flag As Inappropriate" ,{:controller => "admin/rating_flags", :action => "create",:business => b},:method => :post
+	  	rate = Rate.find_by(rateable_id: b.id) rescue nil
+	  	unless rate.nil?
+	  		link_to "Inappropriate" ,{:controller => "admin/rating_flags", :action => "create",:business => b},:method => :post
+	  	end
 	   else
 	   	"Already Flagged"
 	   end
