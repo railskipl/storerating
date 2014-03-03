6cb3631d6572cbae790bc3ccbb66337403f7d7f1
ankita <ankita@ankita-Inspiron-N5010.(none)>
2014-03-03 11:54:52 +0530

    changes in forums

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,24 @@
-class CommentsController < ApplicationController
-  
-  def create
-    @topic = Topic.find(params[:topic_id])
-    @comment = @topic.comments.create!(params[:comment])
-    redirect_to :back, :notice => "Comment created!"  
-  end  
+class CommentsController < ApplicationController 
 
-   
+def index
+  @comments = Comment.all
+end
+
+def new
+  @comment = Comment.new
+end
+  
+	
+def create
+    @forum = Forum.find(params[:forum_id])
+    @comment = @forum.comments.create!(params[:comment])
+    redirect_to @forum, :notice => "Comment created!"  
+  end
+
 
   def count 
-  	@topic = Topic.find(params[:topic_id])
-  	@comment = @topic.comments.count(params[:comment]) 
+  	@forum = forum.find(params[:forum_id])
+  	@comment = @forum.comments.count(params[:comment]) 
   end  
 end 
 
