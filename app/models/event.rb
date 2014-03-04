7a1e6e1d63f085a1a75feaead6772cd654bdcbee
root <root@piyush-inspiron-620s.kipl.com>
2014-03-04 12:54:02 +0530

    changes in events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,9 @@ class Event
   include Mongoid::Timestamps::Created
   include Mongoid::MultiParameterAttributes
   field :event_name, type: String
-  field :date, type: DateTime
+  field :date, type: Date
   field :description, type: String
-  field :tel, type: String
+  field :tel, type: Integer
   field :status,       type: Boolean
+  validates_numericality_of :tel
 end
