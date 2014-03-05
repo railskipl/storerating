2e838ff87c4890dd6550106179652708b4b65a03
ankita <ankita@ankita-Inspiron-N5010.(none)>
2014-03-05 12:43:51 +0530

    forum

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -3,10 +3,12 @@ class Forum
   include Mongoid::MultiParameterAttributes
   include Mongoid::Timestamps::Created
 
-  has_many :comments
+  has_many :comments 
+  belongs_to :user 
+  field :user_id, type: String
   field :name, type: String  
   field :published_on, type: Date
-  field :content, type: String  
+  field :content, type: String   
   validates_presence_of :name, :content
 end
 
